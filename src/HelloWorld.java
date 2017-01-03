bf1b5199be8a87e85f5f06d20ea770bd4ddfdd95
haliekoehler <koehlerdoom@gmail.com>
2017-01-03 12:19:36 -0600

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by HKoehler on 1/3/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Howdy, world!");
+        System.out.println("All java code lives inside of classes");
+        System.out.println("Our program runs the 'main' method when executed");
+        System.out.println("Goodbye, world!");
+    }
+}
