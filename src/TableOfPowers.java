ef1e08998a9096cfd7ecc1da3a88566e02e6ffa0
haliekoehler <koehlerdoom@gmail.com>
2017-01-04 16:42:01 -0600

    TableOfPowers completed

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -16,34 +16,54 @@ public class TableOfPowers {
 
         Scanner sc = new Scanner(System.in);
 
-        int userInt;
-        int i = 2;
-        int j = 3;
-        String toContinue;
+        String userInput;
+        int number;
 
-        // Prompting User for Number
-        System.out.println("Enter a number: ");
-        userInt = sc.nextInt();
-
-        System.out.println("You entered: " + userInt);
-
-        System.out.println("Square Roots up to " + userInt);
-        System.out.println("");
-
-        // Square Roots
         do {
-            System.out.println(i);
-            i*= i;
-        } while (i <= userInt);
+            System.out.print("Enter Integer: ");
+            number = sc.nextInt();
 
-        System.out.println("");
-        System.out.println("Cubed Numbers up to " + userInt);
-        System.out.println("");
+            System.out.println("Number    | Square    | Cube    ");
+            System.out.println("--------------------------------");
+
+            for (int i = 1; i <= number; i++) {
+                System.out.printf("%10s %10s %10s", i, i*i, i*i*i);
+                System.out.println();
+            }
+            System.out.println("Do you want to continue? yes/no");
+            userInput = sc.next();
+        } while (userInput.equals("yes"));
+
+        System.out.println("Thank you! Bye");
+
+//        int userInt;
+//        int i = 2;
+//        int j = 1;
+//        String toContinue;
+//
+//        // Prompting User for Number
+//        System.out.println("Enter a number: ");
+//        userInt = sc.nextInt();
+//
+//        System.out.println("You entered: " + userInt);
+//
+//        System.out.println("Square Roots up to " + userInt);
+//        System.out.println("");
+//
+//        // Square Roots
+//        do {
+//            System.out.println(i);
+//            i*= i;
+//        } while (i <= userInt);
+//
+//        System.out.println("");
+//        System.out.println("Cubed Numbers up to " + userInt);
+//        System.out.println("");
 
         // Cubed Numbers
 //        do{
 //            System.out.println(j);
-//            (j * j) *= j;
+//            Math.pow(j, 3);
 //        } while (j <= userInt);
     }
 }
